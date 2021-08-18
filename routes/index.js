7323c732435290ed90adaafe5c1e3ac3aa4e5094
otto <5285@mbcpeermade.com>
2021-08-18 19:04:45 +0530

    products list added in index.js to act as a database

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let products =[
+    {
+      name:"Iphone 12 Pro",
+      category:"Mobiles",
+      description:"Made by apple",
+      image:"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone-12-pro-202108?wid=400&hei=500&fmt=p-jpg&qlt=95&.v=1628098907000"
+
+    },{
+      name:"Iphone 11",
+      category:"Mobiles",
+      description:"Made by apple",
+      image:"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone-11-202108?wid=400&hei=500&fmt=p-jpg&qlt=95&.v=1628098906000"
+
+
+    },{
+      name:"Iphone XR",
+      category:"Mobiles",
+      description:"Made by apple",
+      image:"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone-XR-202108?wid=400&hei=500&fmt=p-jpg&qlt=95&.v=1628098907000"
+
+    },{
+      name:"Iphone SE",
+      category:"Mobiles",
+      description:"Made by apple",
+      image:"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone-SE-202108?wid=400&hei=500&fmt=p-jpg&qlt=95&.v=1628098903000"
+
+    }
+  ]
+  res.render('index', { products });
+});
+
+module.exports = router;
